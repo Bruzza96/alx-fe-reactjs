9140c066338cf7f9bc1af6e65762bb447fe6f3b4
bruce maumo <bmalimb1@gmail.com>
2025-11-02 17:12:03 +0300

    Fix folder name and component exports

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,9 +1,7 @@
-function Header() {
-    return (
-        <header>
-            <h1>My favourite Cities</h1>
-        </header>
-    );
+export default function Header() {
+  return (
+    <header>
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
 }
-
-export default Header;
