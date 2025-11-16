fcba94d886233d76dd99038ed46fd601bf1edd0a
bruce maumo <bmalimb1@gmail.com>
2025-11-16 18:17:40 +0300

    Update DeleteRecipeButton and EditRecipeForm components

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -3,6 +3,10 @@ import { useRecipeStore } from "./recipeStore";
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
 
+  const handleDelete = () => {
+    deleteRecipe(recipeId);
+  };
+
   return (
     <button onClick={() => deleteRecipe(recipeId)}>
       Delete Recipe
