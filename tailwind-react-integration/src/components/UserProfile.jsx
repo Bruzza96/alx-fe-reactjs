62a7ac64669b2ec2781334b9e3062570cd026e73
bruce maumo <bmalimb1@gmail.com>
2025-11-28 10:33:21 +0300

    Added required responsive image sizing classes sm:w-24 sm:h-24 md:w-36 md:h-36

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -16,8 +16,9 @@ function UserProfile() {
         alt="User"
         className="
         rounded-full
-         w-36 h-36 
-         mx-auto
+        sm:w-24 sm:h-24
+        md:w-36 h-36 
+        mx-auto
         hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="
